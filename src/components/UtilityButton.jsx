93138ab545f03d0ec1958d8a1183e6df75fc1a31
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-01-18 16:06:43 +0530

    refactor: add margin to utillity buttons

--- src/components/UtilityButton.jsx
+++ src/components/UtilityButton.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const UtilityButton = ({ utility }) => {
   return (
-    <button className="col-span-2 text-xl font-medium uppercase bg-teal-500 rounded-full font- text-cyan-100">{utility}</button>
+    <button className="col-span-2 m-1 text-xl font-medium uppercase bg-teal-500 rounded-full text-cyan-100">{utility}</button>
   );
 };
 
